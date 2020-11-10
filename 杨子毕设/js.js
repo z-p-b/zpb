86e063d5e7a1db23298bbce2fddf26b30bd09a40
较大 <164828204@qq.com>
2020-11-10 21:55:52 +0800

    项目上传测试

---
+++ 杨子毕设/js.js
@@ -0,0 +1,96 @@
+ // document.getElementById('ifocus').focus()
+ var i = document.getElementsByClassName('focus')[0]
+
+ function ifocus() {
+     i.style.display = "block"
+ }
+ function ibulrs() {
+     i.style.display = "none"
+ }
+
+ document.getElementById('a').onmousemove = function () {
+     document.getElementById('a').style.color = "#00BE06"
+ }
+ document.getElementById('a').onmouseout = function () {
+     document.getElementById('a').style.color = '#66665B'
+ }
+
+ function show() {
+     document.getElementsByClassName("vipt")[0].style.display = "block";
+ }
+ function leave() {
+     document.getElementsByClassName("vipt")[0].style.display = "none"
+ }
+ function show1() {
+     document.getElementsByClassName("fiff")[0].style.display = "block";
+ }
+ function leave1() {
+     document.getElementsByClassName("fiff")[0].style.display = "none"
+ }
+ function show2() {
+     document.getElementsByClassName("cs")[0].style.display = "block";
+ }
+ function leave2() {
+     document.getElementsByClassName("cs")[0].style.display = "none"
+ }
+ function show3() {
+     document.getElementsByClassName("new")[0].style.display = "block";
+ }
+ function leave3() {
+     document.getElementsByClassName("new")[0].style.display = "none"
+ }
+ function show4() {
+     document.getElementsByClassName("le")[0].style.display = "block";
+ }
+ function leave4() {
+     document.getElementsByClassName("le")[0].style.display = "none"
+ }
+ function show5() {
+     document.getElementsByClassName("land")[0].style.display = "block";
+ }
+ function leave5() {
+     document.getElementsByClassName("land")[0].style.display = "none"
+ }
+ document.getElementsByClassName('vipt')[0].onmousemove = function () {
+     document.getElementsByClassName("vipt")[0].style.display = "block";
+ }
+ document.getElementsByClassName('vipt')[0].onmouseout = function () {
+     document.getElementsByClassName("vipt")[0].style.display = "none";
+ }
+ document.getElementsByClassName('fiff')[0].onmousemove = function () {
+     document.getElementsByClassName("fiff")[0].style.display = "block";
+ }
+ document.getElementsByClassName('fiff')[0].onmouseout = function () {
+     document.getElementsByClassName("fiff")[0].style.display = "none";
+ }
+ document.getElementsByClassName('cs')[0].onmousemove = function () {
+     document.getElementsByClassName("cs")[0].style.display = "block";
+ }
+ document.getElementsByClassName('cs')[0].onmouseout = function () {
+     document.getElementsByClassName("cs")[0].style.display = "none";
+ }
+ document.getElementsByClassName('new')[0].onmousemove = function () {
+     document.getElementsByClassName("new")[0].style.display = "block";
+ }
+ document.getElementsByClassName('new')[0].onmouseout = function () {
+     document.getElementsByClassName("new")[0].style.display = "none";
+ }
+ document.getElementsByClassName('le')[0].onmousemove = function () {
+     document.getElementsByClassName("le")[0].style.display = "block";
+ }
+ document.getElementsByClassName('le')[0].onmouseout = function () {
+     document.getElementsByClassName("le")[0].style.display = "none";
+ }
+ document.getElementsByClassName('land')[0].onmousemove = function () {
+     document.getElementsByClassName("land")[0].style.display = "block";
+ }
+ document.getElementsByClassName('land')[0].onmouseout = function () {
+     document.getElementsByClassName("land")[0].style.display = "none";
+ }
+
+ login_x = function () {
+     document.getElementById('denglu').style.display = "none"
+ }
+ land = function () {
+     document.getElementById('denglu').style.display = "block"
+ }
